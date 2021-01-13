d4ac03da3e65b227292f7fe2c8fabae4689bac2b
Garlic Fiend <suzannaakins@gmail.com>
2021-01-13 12:35:38 -0800

    new mutations

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const { ApolloServer } = require('apollo-server-express');
 //import typeDefs + resolvers
 const { typeDefs, resolvers } = require('./schemas');
 
+//import authorization middleware function
+const { authMiddleware } = require('./utils/auth')
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -13,7 +16,8 @@ const app = express();
 //create new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 //integrate Apollo server w/ Express application as middleware
